52c922213d80ee60d6b3f8859b63510fdfb9f6d2
админ <admin@mac-mini-admin.specialist.ru>
2017-07-10 19:12:09 +0300

    Initial Commit

---
+++ Day2_TableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day2_TableViewDemo
+//
+//  Created by админ on 10.07.17.
+//  Copyright © 2017 админ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
